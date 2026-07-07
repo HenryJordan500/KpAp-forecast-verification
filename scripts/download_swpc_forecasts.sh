c8bc82a1bc6942f85c95d7cc54241129ca37e58a
HenryJordan500 <henry.jordan@colorado.edu>
2026-07-07 16:24:18 -0600

    Started analysis will finish on different branch

--- scripts/download_swpc_forecasts.sh
+++ scripts/download_swpc_forecasts.sh
@@ -1,14 +1,14 @@
 # geomag_forecast
 wget -r -np -nd -A "*.txt" -R "robots.txt" -P raw_data/geomag_forecast https://www.ngdc.noaa.gov/stp/space-weather/swpc-products/daily_reports/geomag_forecast/
 
-# three_day_forecast
-wget -r -np -nd -A "*.txt" -R "robots.txt" -P raw_data/three_day_forecast https://www.ngdc.noaa.gov/stp/space-weather/swpc-products/daily_reports/3day_forecast/
+# # three_day_forecast
+# wget -r -np -nd -A "*.txt" -R "robots.txt" -P raw_data/three_day_forecast https://www.ngdc.noaa.gov/stp/space-weather/swpc-products/daily_reports/3day_forecast/
 
-# daypre
-wget -r -np -nd -A "*.txt" -R "robots.txt" -P raw_data/daypre https://www.ngdc.noaa.gov/stp/space-weather/swpc-products/daily_reports/daypre/
+# # daypre
+# wget -r -np -nd -A "*.txt" -R "robots.txt" -P raw_data/daypre https://www.ngdc.noaa.gov/stp/space-weather/swpc-products/daily_reports/daypre/
 
-# the_weekly
-wget -r -np -nd -A "*.pdf" -R "Usr_guide.pdf" -P raw_data/the_weekly https://www.ngdc.noaa.gov/stp/space-weather/swpc-products/weekly_reports/PRFs_of_SGD/
+# # the_weekly
+# wget -r -np -nd -A "*.pdf" -R "Usr_guide.pdf" -P raw_data/the_weekly https://www.ngdc.noaa.gov/stp/space-weather/swpc-products/weekly_reports/PRFs_of_SGD/
 
-# RSGA
-wget -r -np -nd -A "*.pdf, *.txt" -R "Usr_guide.pdf, robots.txt" -P raw_data/rsga https://www.ngdc.noaa.gov/stp/space-weather/swpc-products/daily_reports/reports_solar_geophysical_activity/
+# # RSGA
+# wget -r -np -nd -A "*.pdf, *.txt" -R "Usr_guide.pdf, robots.txt" -P raw_data/rsga https://www.ngdc.noaa.gov/stp/space-weather/swpc-products/daily_reports/reports_solar_geophysical_activity/
